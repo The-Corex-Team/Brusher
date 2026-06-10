1828635728a1eff2c3910da300c0c7ae62e3143c
walter-322 <walter.muller.1708@gmail.com>
2026-06-10 19:06:41 +0200

    new!

--- build.sh
+++ build.sh
@@ -1,18 +1,5 @@
-#!/bin/bash
-
-set -e
-
-echo "Creating build directory..."
-mkdir -p build
-
-echo "Entering build directory..."
-cd build
-
-echo "Configuring project..."
-cmake ..
-
-echo "Building Brusher..."
-cmake --build . -j$(nproc)
-
-echo "Running Brusher..."
-./Brusher
+cd ~/Projects/Brusher
+rm -rf build
+cmake -B build -DCMAKE_BUILD_TYPE=Release
+cmake --build build --config Release
+./build/Brusher
